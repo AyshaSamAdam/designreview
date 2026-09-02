985f696078dc7f395959d300f33ec3cd634dac8f
AyshaSamAdam <ayesha.btx09@gmail.com>
2026-09-01 23:31:01 -0400

    feat: prisma setup with User Model, migration applied

--- Backened/services/auth-service/src/server.ts
+++ Backened/services/auth-service/src/server.ts
@@ -4,7 +4,8 @@ import "dotenv/config"
 import {z } from 'zod'
 
 const envSchema = z.object({
-    PORT : z.coerce.number()
+    PORT : z.coerce.number(),
+    DATABASE_URL : z.string().url()
 })
 
 
