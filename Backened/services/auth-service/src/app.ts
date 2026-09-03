4165671ab6d35946e96f8a6181372dfcc27861da
AyshaSamAdam <ayesha.btx09@gmail.com>
2026-09-03 05:17:25 -0400

    feat : helmet and cors added to auth-service and diagram-service

--- Backened/services/auth-service/src/app.ts
+++ Backened/services/auth-service/src/app.ts
@@ -1,7 +1,16 @@
 import  express from "express";
 import authRoutes  from './routes/authRoutes.js'
+import helmet from "helmet";
+import cors from "cors"
+
+
 
 const app = express();
+app.use(helmet())
+app.use(cors({
+    origin : "http://localhost:3000",
+    credentials : true
+}))
 
 
 app.use(express.json());
